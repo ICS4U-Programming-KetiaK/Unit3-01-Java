094fc62828b923a949efac93f8bc7f4c44cf40f3
Ubuntu <ubuntu@ip-172-31-29-30.ec2.internal>
2022-05-12 13:01:16 +0000

    Created empty constructors.

---
+++ Main.java
@@ -0,0 +1,25 @@
+/**
+* Creating the main function.
+*
+* @author Ketia Gaelle Kaze
+* @version 1.0
+* @since 2022-04-25
+*/
+class Main {
+
+    /**
+    * Creating an empty constructor for main.
+    */
+    public main() { }
+    /**
+    * Creating a main function.
+    */
+
+    public static void main(String[] args) {
+        // creating an new object for the integer stack
+        MyIntStack myIntStack = new MyIntStack();
+
+        // creating a new object for the string stack
+        MyStringStack myStringStack = new MyStringStack();
+    }
+}
---
+++ MyStringStack.java
@@ -0,0 +1,14 @@
+/**
+* Creating an empty constructor.
+*
+* @author Ketia Gaelle Kaze
+* @version 1.0
+* @since 2022-04-25
+*/
+
+class MyStringStack {
+    /**
+    * Creating an empty constructor.
+    */
+    public MyStringStack() { }
+}
